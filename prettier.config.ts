de1a2a4da91f5ec016d1c012eef4cc7089e86adf
Anthony Hagidimitriou <info@anthonyhagi.com.au>
2024-07-13 14:47:48 +0930

    chore: add prettier config

---
+++ prettier.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+  semi: true,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: "es5",
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
+
+export default config;
